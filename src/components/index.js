14dbe6d280fb8a554a09f92ffae73bcaecaaa009
Adnan <E22CSEU1269@Bennett.edu.in>
2025-08-15 00:16:39 +0530

    fix import path for Header component

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-import Header from "./header/Header";
+import Header from "./Header/Header";
 import Footer from "./footer/Footer";
 import Container from "./container/Container"
 import Logo from "./Logo";
