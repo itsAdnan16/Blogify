ca837478442b40b2b8e8386a75ce0c28692a7671
Adnan <E22CSEU1269@Bennett.edu.in>
2025-08-15 00:52:05 +0530

    fixed the container bug

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-import Container from "./container/Container"
+import Container from "./Container"
 import Logo from "./Logo";
 import Button from './Button'
 import Input from './Input'
