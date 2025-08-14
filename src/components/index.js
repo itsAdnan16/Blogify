33b02657301b90b904a2c7841b50aac8cd3ded57
Adnan <E22CSEU1269@Bennett.edu.in>
2025-08-15 01:30:50 +0530

    refactor imports in index.js for consistency and clarity

--- src/components/index.js
+++ src/components/index.js
@@ -1,30 +1,30 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-import Container from "./Container"
+import Container from "./Container";
 import Logo from "./Logo";
-import Button from './Button'
-import Input from './Input'
-import LogoutBtn from "./Header/LogoutBtn"
+import Button from "./Button";
+import Input from "./Input";
+import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
-import Select from './Select'
-import Signup from './Signup'
-import Login from './Login'
-import PostCard from './PostCard'
-import PostForm from './post-form/PostForm'
-import Protected from './AuthLayout'
-export { 
-    Header, 
-    Footer, 
-    Container, 
-    Logo, 
-    Button,
-    Input,
-    Select,
-    LogoutBtn, 
-    RTE, 
-    Signup, 
-    Login, 
-    PostCard,
-    PostForm,
-    Protected,
+import Select from "./Select";
+import Signup from "./Signup";
+import Login from "./Login";
+import PostCard from "./PostCard";
+import PostForm from "./post-form/PostForm";
+import Protected from "./AuthLayout";
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  Button,
+  Input,
+  Select,
+  LogoutBtn,
+  RTE,
+  Signup,
+  Login,
+  PostCard,
+  PostForm,
+  Protected,
 };
